c63c6dda4d9cf538000295fe20a29dbe23b06233
Shiva040912 <shivaarun0071@gmail.com>
2026-07-18 17:37:04 +0530

    Fix CORS for Vercel

--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,20 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL,
+    origin: [
+      'http://localhost:5173',
+      'https://crackerkart-fronend-ogog.vercel.app',
+    ],
     methods: 'GET,POST,PUT,PATCH,DELETE',
     credentials: true,
   });
 
- app.useGlobalPipes(
-   new ValidationPipe({
-    whitelist: true,
-    transform: true,
-    forbidNonWhitelisted: true,
-   }),
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
   );
 
   await app.listen(process.env.PORT ?? 3000);
